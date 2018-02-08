0b4dbb1155759f6281e2acb2ac3828df45c35a35
geeksainik <sairam9.0m@gmail.com>
2018-02-08 20:37:09 +0530

    new branch added

--- src/sampleGIT/Samplegit.java
+++ src/sampleGIT/Samplegit.java
@@ -5,6 +5,8 @@ public class Samplegit {
 	public static void main(String[] args) {
 
 		System.out.println("thisissample");
+		System.out.println("branch added");
+		System.out.println("how it works?");
 	}
 
 }
